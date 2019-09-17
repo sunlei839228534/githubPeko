4058b3957c86ee4468cec835db18504ab0bdfcb9
孙磊 <839228534@qq.com>
2019-09-17 23:38:13 +0800

    add

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,14 @@
 import {withRouter} from 'next/router'
 
+/**
+ * 
+ * sort: 排序方式
+ * order: 排序的顺序
+ * lang: 仓库的项目开发主语言
+ * page: 分页页面 
+ * 
+ */
+
 function Search({router}) {
   return <span>{router.query.query}</span>
 }
